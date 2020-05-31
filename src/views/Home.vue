a3e5fe35a02d24fa657ab6d28e0795203b57c058
Hossein2x <hrakbari77@gmail.com>
2020-05-31 12:22:21 +0430

    semantic-ui-css added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <button class="ui primary button">OK</button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
