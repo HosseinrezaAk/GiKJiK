6aca72d0346ab29daf793a522526a53639a173e7
Hossein2x <hrakbari77@gmail.com>
2020-06-05 23:19:06 +0430

    Entrance.vue added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <v-app>
-    
-
     <v-content>
-      <router-view></router-view>
+      <app-entrance></app-entrance>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
+// import Home from './views/Home'
+import Entrance from './views/Entrance'
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    
+    appEntrance : Entrance
+
   },
 
   data: () => ({
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
-
+import Entrance from '../views/Entrance.vue'
 
 Vue.use(VueRouter)
 
@@ -19,6 +18,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path:'/entrance',
+    name: 'Entrance',
+    component:Entrance
   }
 
   
