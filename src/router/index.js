ab4a6845832fea808eebfed8e9cf8e8e2f6d7463
Hossein2x <hrakbari77@gmail.com>
2020-06-05 19:30:21 +0430

    login and sign up fixed

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
-import Sign_in from '../components/Sign_in'
-import Sign_up from '../components/Sign_up'
+
 
 Vue.use(VueRouter)
 
@@ -20,17 +19,9 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path:'/Sign_in',
-    name:'Sign_in',
-    component:Sign_in
-  },
-  {
-    path:'/Sign_up',
-    name:'Sign_up',
-    component:Sign_up
   }
+
+  
 ]
 
 const router = new VueRouter({
