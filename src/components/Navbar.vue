da5681fde1428906e433c34e03b72b3e9a8167c3
Hossein2x <hrakbari77@gmail.com>
2020-06-07 00:53:47 +0430

    drawer items added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,15 +17,25 @@
             </v-btn>
         </v-toolbar>
         <v-navigation-drawer app 
-        class="teal accent-4"
-        color="teal"
-        v-model="drawer"
-        absolute
-        bottom
-        temporary
-         >
-            <p> test </p>
-
+            class="teal accent-5"
+            color="teal"
+            v-model="drawer"
+            
+            bottom
+            temporary
+                >
+            <v-list dense nav>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">
+                            {{ link.text }}
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
         </v-navigation-drawer>
     </div>
 </template>
@@ -34,7 +44,13 @@
     export default {
         data() {
             return {
-                drawer: false
+                drawer: false,
+                links: [
+                    { icon: 'dashboard', text: 'Dashboard', route: '/'},
+                    { icon: 'folder', text: 'My Projects', route: '/home'},
+                    { icon: 'person', text: 'Team', route: '/about'}
+                    
+                ]
             }
         },
         
