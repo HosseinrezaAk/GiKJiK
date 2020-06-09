a7c4f8fac5f6186f9e1e4348bb473d603e22b78c
hrakbari77@gmail.com <hrakbari77@gmail.com>
2020-06-10 03:21:30 +0430

    Quiz .vue created

--- src/components/ClassNavbar.vue
+++ src/components/ClassNavbar.vue
@@ -42,32 +42,17 @@
                     </v-flex>
                 </v-layout>
 
-            <v-list class="ml-2" dense nav>
-                <v-dialog   max-width="600px" class="mb-5" v-model="dialog" v-for="pop in popups" :key="pop.text">
-                    <template v-slot:activator="{on,attrs}">
-                        
-                        <v-btn small dark text v-on="on" v-bind="attrs" class="teal accent-5 mb-5 " >
-					    <v-icon left>{{pop.icon}}</v-icon>
-                        <span class="nav text-capitalize ml-6 line-height-1.2">{{pop.text}}</span>
-
-				        </v-btn>
-                    </template>
-                    <v-card>
-                        <v-card-title>
-                            <h2>Add a New Class</h2>
-                        </v-card-title>
-                        <v-card-text>
-                            <v-form class="px-3" ref="form">
-                                <v-text-field label="Title" v-model="title" prepend-icon="title" :rules="inputRules"></v-text-field>
-                                <v-textarea label="information" v-model="content" prepend-icon="edit"></v-textarea>
-                                <v-btn text class="success  mt-3" @click="submit" :loading="loading">Add Class</v-btn>
-                            </v-form>
-                        </v-card-text>
-
-                    </v-card>
-                </v-dialog>
-
-                
+            <v-list dense nav>
+                <v-list-item  v-for="pop in popups" :key="pop.text" router :to="pop.route">
+                    <v-list-item-action>
+                        <v-icon class="white--text">{{ pop.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">
+                            {{ pop.text }}
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
             </v-list>
         </v-navigation-drawer>
     </div>
@@ -83,9 +68,10 @@
             return {
                 drawer: true,
                 popups: [
-                    { icon: 'supervised_user_circle', text: 'Members'},
-                    { icon: 'check_circle_outline', text: 'Make a Quiz' },
-                    { icon: 'announcement', text: 'News'}
+                   
+                    { icon: 'check_circle_outline', text: 'Make a Quiz',route:'/about' },
+                    { icon: 'aspect_ratio', text: 'White board',route:'/user'},
+                    { icon: 'announcement', text: 'News',route:''}
                     
                 ],
                 dialog:false
---
+++ src/components/Quiz.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="quiz">
+        <p>HELLLO this is Quiz</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
